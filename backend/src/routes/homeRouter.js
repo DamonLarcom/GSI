f102cba7d40b1bec82d05c5f1f8b65f3374795ff
Jordan <JordanR5a@users.noreply.github.com>
2020-11-17 12:11:49 -0500

    subdomain routes

--- backend/src/routes/homeRouter.js
+++ backend/src/routes/homeRouter.js
@@ -6,5 +6,13 @@ module.exports = () => {
 	.get((req, res) => {
 		res.send("home");
 	});
+	homeRouter.route("/signup")
+	.post((req, res) => {
+		// create user object from params, save in user collection, log the user in, and direct them to home page
+	});
+	homeRouter.route("/login")
+	.get((req, res) => {
+		// get user details from form and log them in, direct them to home page 
+	});
 	return homeRouter;
 }
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -6,5 +6,24 @@ module.exports = () => {
 	.get((req, res) => {
 		res.send("user");
 	});
+	userRouter.route("/blockToggle")
+	.put((req, res) => {
+		// Header contains UserID named Blocker and another UserID named Blockee
+	});
+	userRouter.route("/followToggle")
+	.put((req, res) => {
+		// edit both the follower and the followee to reflect new following
+	});
+	userRouter.route("/:userId")
+	.put((req, res) => {
+		// Body contains JSON object with a User object 
+	})
+	.delete((req, res) => {
+		// Deletes the user from the database with the path param id and redirects to log in page.
+	});
+	userRouter.route("/logout")
+	.get((req, res) => {
+		// logs user out and redirects the page to the sign in page 
+	});
 	return userRouter;
 }
