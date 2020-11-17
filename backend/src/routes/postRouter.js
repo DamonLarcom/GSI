d620e34f5c0cb8c6649378255995d9c38ead9b65
Jordan <JordanR5a@users.noreply.github.com>
2020-11-17 12:33:30 -0500

    basic res.render

--- backend/src/routes/postRouter.js
+++ backend/src/routes/postRouter.js
@@ -4,14 +4,16 @@ const postRouter = express.Router();
 module.exports = () => {
 	postRouter.route("/")
 	.get((req, res) => {
-		res.send("post");
+		res.render('createPostPage', {});
 	})
 	.post((req, res) => {
 		// Creates a new post with the post as the body 
+		res.render('createPostPage', {});
 	});
 	postRouter.route("/:postId")
 	.patch((req, res) => {
 		// Edits a post with the post as the body
+		res.render('editPostPage', {});
 	})
 	.delete((req, res) => {
 		// Deletes a post with the matching PostID in the path
@@ -19,14 +21,17 @@ module.exports = () => {
 	postRouter.route("/likeToggle/:postId")
 	.patch((req, res) => {
 		// update post object numLike and user who liked
+		res.render('postDisplay', {});
 	});
 	postRouter.route("/comment/:postId")
 	.patch((req, res) => {
 		// add a comment on a post
+		res.render('postDisplay', {});
 	});
 	postRouter.route("/deleteCom/:postId")
 	.patch((req, res) => {
 		// delete comment from post
+		res.render('postDisplay', {});
 	});
 	
 	return postRouter;
