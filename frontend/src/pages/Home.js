8ef4d0781e993f9c64ba0d6a136a34af9ae6a6b5
CarterWilde <carterjwilde@gmail.com>
2020-11-18 10:19:46 -0700

    Created post

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Post from "../components/Post";
 
 export const Home = (props) => {
     const fullUser = props.user.name + ":" + props.user.password;
@@ -8,6 +9,7 @@ export const Home = (props) => {
             {true ? <p>This is </p> : <p>Not Here</p>}
             <p>Hello Home{props.title}{fullUser}</p>
             <button onClick={() => {props.onTest()}}>Test</button>
+            <Post text="Something" username="Carter" userId={25}/>
         </div>
     );
 }
