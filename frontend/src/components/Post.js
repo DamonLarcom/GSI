cd49c754bea38c8317c79bacb90e038093b92aa3
CarterWilde <carterjwilde@gmail.com>
2020-11-23 09:45:08 -0700

    Start of delete post.

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,16 +1,21 @@
 //@ts-check
 import React from "react"
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 
 /**
  * @param {{ username: String; text: String; userId: any }} props
  */
 const Post = props => {
+    const deletePost = () => {
+
+    }
+
     return (
         <Card>
             <Card.Body>
                 <Card.Title>Posted by {props.username}</Card.Title>
                 <Card.Text>{props.text}</Card.Text>
+                <Button variant="danger" onClick={deletePost}>Delete</Button>
             </Card.Body>
         </Card>
     );
