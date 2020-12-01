3e9aae0fd7b3aead76677619d591c91dad22164e
Cole Damon <coledamon2001@gmail.com>
2020-12-01 10:02:44 -0700

    search functionality

--- backend/src/routes/searchRouter.js
+++ backend/src/routes/searchRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const searchRouter = express.Router();
 const User = require("../models/user")
+const Post = require("../models/post")
 
 module.exports = () => {
 	
@@ -11,13 +12,19 @@ module.exports = () => {
 	searchRouter.route("/user")
 	.get((req, res) => {
 		// get the search term and search the user collection for a partial match on usernames and re-render the page with results 
-		let searchText = req.body.searchText;
-		
+		User.find({"username": {"$regex": new RegExp(req.body.searchText, "i")}}, (err, users) => {
+			if(err) console.error(err);
+			res.json(users);
+		})
 	});
 
 	searchRouter.route("/keyword")
 	.get((req, res) => {
 		// get the search term and search the post collection for a partial match on post text and re-render the page with results
+		Post.find({"text": {"$regex": new RegExp(req.body.searchText, "i")}}, (err, posts) => {
+			if(err) console.error(err);
+			res.json(posts);
+		})
 	});
 
 	return searchRouter;
