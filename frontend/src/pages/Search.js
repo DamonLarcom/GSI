d52e5547ec635d3647fa44ce21538dec1b85c223
WinnieQueen <ashilliard@student.neumont.edu>
2020-12-04 19:25:49 -0700

    added support for search by keyword

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -1,6 +1,7 @@
 //@ts-check
 import React from "react";
 import axios from "axios";
+import Post from "../components/Post";
 
 import {Button, Form, InputGroup, FormControl} from "react-bootstrap";
 
@@ -9,17 +10,23 @@ export default class Search extends React.Component {
         super(props)
         this.state = {value: '', results: []};
         this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUserSubmit = this.handleUserSubmit.bind(this);
+        this.handleKeywordSubmit = this.handleKeywordSubmit.bind(this);
     }
 
     handleChange(e) {
         this.setState({...this.state, value: e.target.value});
     }
 
-    async handleSubmit(e) {
+    async handleUserSubmit(e) {
         let searchParam = this.state?.value;
-        const posts = await (await axios.put(`${process.env.BACKEND_URL}/search/user`, {searchText: this.state.value})).data
-        var usersList = posts; //get the list from the backend
+        const posts = await (await axios.put(`${process.env.BACKEND_URL}/search/keyword`, {searchText: this.state.value})).data;
+        this.setState({...this.state, results: posts});
+    }
+
+    async handleKeywordSubmit(e) {
+        let searchParam = this.state?.value;
+        const usersList = await (await axios.put(`${process.env.BACKEND_URL}/search/user`, {searchText: this.state.value})).data;
         this.setState({...this.state, results: usersList});
     }
 
@@ -29,13 +36,18 @@ export default class Search extends React.Component {
                 <InputGroup>
                     <FormControl type="text" placeholder="Search" onChange={this.handleChange}/>
                     <InputGroup.Append>
-                        <Button onClick={this.handleSubmit}>Search</Button>
+                        <Button style={{marginLeft: "1%"}} onClick={this.handleUserSubmit}>Search For Users</Button>
+                        <Button style={{marginLeft: "1%"}} onClick={this.handleKeywordSubmit}>Search For Posts</Button>
                     </InputGroup.Append>
                 </InputGroup>
                 <ul>
-                    {this.state.results.map(function(User) {
-                        return <li key={User._id}><a href={`/#/profile/${User._id}/view`}>{User.username}</a></li>
-                    })}
+                    {this.state.results.map(function(Object) {
+                        if(Object.hasOwnProperty('password')) {
+                            return <li key={Object._id}><a href={`/#/profile/${Object._id}/view`}>{Object.username}</a></li>
+                        } else {
+                            return(<Post text={Object.text} userId={Object.user} username={Object.username} postId={Object._id} key={Object._id}/>);
+                        }
+                    })} 
                 </ul>
             </Form>
         );
