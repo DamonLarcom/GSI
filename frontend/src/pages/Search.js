1c258fcddc3caa9c895b5ee4e2f79dd02f5679b3
WinnieQueen <ashilliard@student.neumont.edu>
2020-12-08 01:00:50 -0700

    Doesnt show blocked users in searches

--- frontend/src/pages/Search.js
+++ frontend/src/pages/Search.js
@@ -3,51 +3,61 @@ import React from "react";
 import axios from "axios";
 import Post from "../components/Post";
 
-import {Button, Form, InputGroup, FormControl, ButtonGroup} from "react-bootstrap";
+import { Button, Form, InputGroup, FormControl, ButtonGroup } from "react-bootstrap";
+
+
+/**
+ * @param {{ userid: String; }} props
+ */
 
 export default class Search extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {value: '', results: []};
+        this.state = { value: '', results: [], currentUser: null };
         this.handleChange = this.handleChange.bind(this);
         this.handleUserSubmit = this.handleUserSubmit.bind(this);
         this.handleKeywordSubmit = this.handleKeywordSubmit.bind(this);
     }
 
     handleChange(e) {
-        this.setState({...this.state, value: e.target.value});
+        this.setState({ ...this.state, value: e.target.value });
     }
 
     async handleKeywordSubmit(e) {
         let searchParam = this.state?.value;
-        const posts = await (await axios.put(`${process.env.BACKEND_URL}/search/keyword`, {searchText: this.state.value})).data;
-        this.setState({...this.state, results: posts});
+        const posts = await (await axios.put(`${process.env.BACKEND_URL}/search/keyword`, { searchText: this.state.value })).data;
+        const user = await (await axios.get(`${process.env.BACKEND_URL}/user/${this.props.userid}`)).data;
+        this.setState({ ...this.state, results: posts, currentUser: user });
     }
 
     async handleUserSubmit(e) {
         let searchParam = this.state?.value;
-        const usersList = await (await axios.put(`${process.env.BACKEND_URL}/search/user`, {searchText: this.state.value})).data;
-        this.setState({...this.state, results: usersList});
+        const usersList = await (await axios.put(`${process.env.BACKEND_URL}/search/user`, { searchText: this.state.value })).data;
+        this.setState({ ...this.state, results: usersList });
     }
 
     render() {
-        return(
-            <Form style={{margin: "5%"}}>
+        return (
+            <Form style={{ margin: "5%" }}>
                 <InputGroup>
-                    <FormControl type="text" placeholder="Search" onChange={this.handleChange}/>
+                    <FormControl type="text" placeholder="Search" onChange={this.handleChange} />
                     <InputGroup.Append>
-                            <Button onClick={this.handleUserSubmit}>Search For Users</Button>
-                            <Button onClick={this.handleKeywordSubmit}>Search For Posts</Button>
+                        <Button onClick={this.handleUserSubmit}>Search For Users</Button>
+                        <Button onClick={this.handleKeywordSubmit}>Search For Posts</Button>
                     </InputGroup.Append>
                 </InputGroup>
                 <ul>
-                    {this.state.results.map(function(Object) {
-                        if(Object.hasOwnProperty('profile')) {
-                            return <li key={Object._id}><a href={`/#/profile/${Object._id}/view`}>{Object.username}</a></li>
+                    {this.state.results.map(function (Object) {
+                        if (Object.hasOwnProperty('profile')) {
+                            if (!Object.blockedBy.includes(this.props.userid) && !Object.blockedUsers.includes(this.props.userid)) {
+                                return <li key={Object._id}><a href={`/#/profile/${Object._id}/view`}>{Object.username}</a></li>
+                            }
                         } else {
-                            return(<Post text={Object.text} userId={Object.user} username={Object.username} postId={Object._id} key={Object._id}/>);
+                            if (this.state.currentUser != null && !this.state.currentUser.user.blockedBy.includes(this.props.userid) && !this.state.currentUser.blockedUsers.includes(this.props.userid)) {
+                                return (<Post text={Object.text} userId={Object.user} username={Object.username} postId={Object._id} key={Object._id} />);
+                            }
                         }
-                    })} 
+                    })}
                 </ul>
             </Form>
         );
