de3bc37d77e73a195ff59443cbfeb0a2a6f0ee94
CarterWilde <carterjwilde@gmail.com>
2020-12-08 11:56:14 -0700

    Update User info

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -13,6 +13,7 @@ class Profile extends React.Component {
         this.getProfile = this.getProfile.bind(this);
         this.handleFollow = this.handleFollow.bind(this);
         this.handleBlock = this.handleBlock.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
 
         this.state = {
             user: {
@@ -25,37 +26,38 @@ class Profile extends React.Component {
                 followedUsers: [],
                 authoredPosts: [],
                 posts: []
-            }
+            },
+            showDelete: false,
         }
-        this.handleDelete = this.handleDelete.bind(this);
         this.getProfile();
     }
 
     getProfile() {
         axios.get(`${process.env.BACKEND_URL}/user/${this.props.match.params.userId}`)
-        .then(res => {
-            this.setState({user:{...this.state.user, ...res.data.profile, authoredPosts: res.data.authoredPosts, username: res.data.username}});
-            this.forceUpdate();
-            this.getPosts();
-        });
+            .then(res => {
+                console.log(res.data);
+                this.setState({ user: { ...this.state.user, ...res.data.profile, authoredPosts: res.data.authoredPosts, username: res.data.username }});
+                this.forceUpdate();
+                this.getPosts();
+            });
     }
 
     async handleDelete() {
         try {
-            const data = await axios.delete(`${process.env.BACKEND_URL}/user/${this.props.match.params.userId}`, {...this.state.input});
-            this.setState({ show: false });
+            const data = await axios.delete(`${process.env.BACKEND_URL}/user/${this.props.match.params.userId}`, { ...this.state.input });
+            this.setState({ showDelete: false });
             window.location = "/"
         } catch (error) {
             console.log("Delete error", error);
-            this.setState({ show: true});
+            this.setState({ showDelete: true });
         }
     }
 
     async handleFollow() {
         try {
             console.log(this.props)
-            const data = await axios.put(`${process.env.BACKEND_URL}/user/followToggle/${this.props.match.params.userId}`, {...this.state.input});
-        } catch(error) {
+            const data = await axios.put(`${process.env.BACKEND_URL}/user/followToggle/${this.props.match.params.userId}`, { ...this.state.input });
+        } catch (error) {
             console.log("Follow error", error);
         }
     }
@@ -63,73 +65,73 @@ class Profile extends React.Component {
     async handleBlock() {
         try {
             console.log(this.props)
-            const data = await axios.put(`${process.env.BACKEND_URL}/user/blockToggle/${this.props.match.params.userId}`, {...this.state.input});
-        } catch(error) {
+            const data = await axios.put(`${process.env.BACKEND_URL}/user/blockToggle/${this.props.match.params.userId}`, { ...this.state.input });
+        } catch (error) {
             console.log("Blocking error", error);
         }
     }
 
     checkFollow() {
-        if(this.props.user.followedUsers.includes(this.props.match.params.userId)) {
+        if (this?.props?.user?.followedUsers?.includes(this.props.match.params.userId)) {
             return true;
         }
         return false;
     }
-    
+
     getPosts() {
         let collection = [];
 
         this.state.user.authoredPosts.forEach(postId => {
             axios.get(`${process.env.BACKEND_URL}/post/${postId}`)
-            .then(res => {
-                collection.push(res.data);
-                this.forceUpdate();
-            })
+                .then(res => {
+                    collection.push(res.data);
+                    this.forceUpdate();
+                })
         });
 
         collection.sort();
 
-        this.setState({user: {...this.state.user, posts: collection}})
+        this.setState({ user: { ...this.state.user, posts: collection } })
     }
 
     render() {
-        return(
+        return (
             !this.props?.user?.profile?.blockedUsers?.includes(this.props.match.params.userId) ? (<>
-                <Modal show={this.state.show} onHide={() => { this.setState({ show: false }) }} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Delete Profile</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <p>Are you sure you want to delete?</p>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => { this.setState({ show: false }) }}>Close</Button>
-                    <Button variant="danger" onClick={this.handleDelete}>Delete</Button>
-                </Modal.Footer>
-            </Modal>
+                <Modal show={this.state.showDelete} onHide={() => { this.setState({ showDelete: false }) }} centered>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Delete Profile</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>Are you sure you want to delete?</p>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => { this.setState({ showDelete: false }) }}>Close</Button>
+                        <Button variant="danger" onClick={this.handleDelete}>Delete</Button>
+                    </Modal.Footer>
+                </Modal>
 
-            <Card style={{margin: "2em", padding: "2em"}}>
-                <Card.Title>{this.state.user.username} {this.state.user.name ? "(" + this.state.user.name + ")": null }</Card.Title>
+                <Card style={{ margin: "2em", padding: "2em" }}>
+                    <Card.Title>{this.state.user.username} {this.state.user.name ? "(" + this.state.user.name + ")" : null}</Card.Title>
                     <Card.Body>
-                        {this.props.user?.email ? <Card.Text>Email: {this.state.user.email}</Card.Text>:null}
-                        {this.props.user?.phoneNum ? <Card.Text>Phone: {this.state.user.phoneNum}</Card.Text>:null}
+                        {this.props.user?.email ? <Card.Text>Email: {this.state.user.email}</Card.Text> : null}
+                        {this.props.user?.phoneNum ? <Card.Text>Phone: {this.state.user.phoneNum}</Card.Text> : null}
                         <Card.Text>{this.state.user.followedBy.length} Followers | {this.state.user.followedUsers.length} Following</Card.Text>
                         <Card.Text>{this.state.user.bio}</Card.Text>
                         <ButtonGroup>
-                            {this.props?.user?._id == this.props.match.params.userId ?  (
+                            {this.props?.user?._id == this.props.match.params.userId ? (
                                 <>
                                     <Button variant="primary" as={NavLink} to={`/profile/${this.props.match.params.userId}/edit`}>Edit Profile</Button>
                                     <Button variant="outline-danger" as={NavLink} to={`/profile/${this.props.match.params.userId}/blocked`}>Blocked Users</Button>
-                                    <Button variant="danger" onClick={() => { this.setState({ show: true }) }}>Delete Profile</Button>
+                                    <Button variant="danger" onClick={() => { this.setState({ showDelete: true }) }}>Delete Profile</Button>
                                 </>
-                            ): (
-                                <>
-                                    <Button variant="primary" onClick={this.handleFollow}>{this.checkFollow() ? "Unfollow" : "Follow"}</Button>
-                                    <Button variant="danger" onClick={this.handleBlock}>Block</Button>
-                                </>
-                            )}    
+                            ) : (
+                                    <>
+                                        <Button variant="primary" onClick={this.handleFollow}>{this.checkFollow() ? "Unfollow" : "Follow"}</Button>
+                                        <Button variant="danger" onClick={this.handleBlock}>Block</Button>
+                                    </>
+                                )}
                         </ButtonGroup>
-                        
+
                         <Accordion>
                             <Card>
                                 <Card.Header>
@@ -139,20 +141,20 @@ class Profile extends React.Component {
                                 </Card.Header>
                                 <Accordion.Collapse eventKey="0">
                                     <>
-                                    {
-                                        this.state.user.posts.map(post => {
-                                            return(<Post key={post._id} username={post.username} userId={post.user} text={post.text} postId={post._id}/>);
-                                        })
-                                    }
+                                        {
+                                            this.state.user.posts.map(post => {
+                                                return (<Post key={post._id} username={post.username} userId={post.user} text={post.text} postId={post._id} />);
+                                            })
+                                        }
                                     </>
                                 </Accordion.Collapse>
                             </Card>
                         </Accordion>
                     </Card.Body>
-            </Card>
+                </Card>
             </>) : <p>Looks like you've been blocked.</p>
         );
     }
 }
 
-export default connect(state=>({ ...state }))(Profile);
+export default connect(state => ({ ...state }))(Profile);
