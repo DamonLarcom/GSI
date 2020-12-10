cc7e5ae2569e7dce49f9328344fedab4977ea40b
CarterWilde <carterjwilde@gmail.com>
2020-12-10 08:37:49 -0700

    fixed Profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -143,8 +143,8 @@ class Profile extends React.Component {
                 <Card style={{margin: "2em", padding: "2em"}}>
                     <Card.Title>{this.state.user.username} {this.state.user.name ? "(" + this.state.user.name + ")": null }</Card.Title>
                         <Card.Body>
-                            {this.props.user?.email ? <Card.Text>Email: {this.state.user.email}</Card.Text>:null}
-                            {this.props.user?.phoneNum ? <Card.Text>Phone: {this.state.user.phoneNum}</Card.Text>:null}
+                            {this.state.user?.email ? <Card.Text>Email: {this.state.user.email}</Card.Text>:null}
+                            {this.state.user?.phoneNum ? <Card.Text>Phone: {this.state.user.phoneNum}</Card.Text>:null}
                             <Card.Text>{this.state.user.followedBy.length} <NavLink to={`/profile/${this.props.match.params.userId}/followedBy`}>Followers</NavLink> | {this.state.user.followedUsers.length} <NavLink to={`/profile/${this.props.match.params.userId}/followed`}>Following</NavLink></Card.Text>
                             <Card.Text>{this.state.user.bio}</Card.Text>
                             {this.checkBlock() ? 
